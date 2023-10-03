5178d64dab4ad3fbd1c8702bc3d9abad2e1f85fb
lekapo <lekapo@gmail.com>
2023-10-03 16:41:21 -0300

    search now is working, and has a fluid container

--- search.php
+++ search.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * The template for displaying search results pages
+ * The template for displaying archive pages
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
  * @package UnderStrap
  */
@@ -10,88 +12,108 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 
-$container = get_theme_mod( 'understrap_container_type' );
-
+$container = get_theme_mod( 'understrap_container_fluid' );
 ?>
 
-<div class="wrapper" id="search-wrapper">
+<div class="wrapper" id="archive-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="row">
+			<div class="col-12 offset-sm-1 col-sm-10 offset-lg-0 col-lg-12 offset-xl-1 col-xl-10">
 
-		<div class="row">
-			<div class="col-12 offset-sm-1 col-sm-10 offset-lg-1 col-lg-10 offset-xl-1 col-xl-10 offset-xxl-1 col-xxl-10">
-				<main class="site-main" id="main">
-
-					<?php
-					if ( have_posts() ) :
-					?>
+				<!-- Latest -->
+				<div id="category">
 					<header class="page-header">
 						<?php
-							echo '<h1>';
-							printf(
-
-								/* translators: %s: query term */
-								esc_html__( 'Resultados da pesquisa por: %s', 'understrap' ),
-								'<span>' . get_search_query() . '</span>'
-							);
-							echo '</h1>';
+						the_archive_title( '<h1 class="page-title">//', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
 					</header><!-- .page-header -->
 
 					<div class="card-columns">
-						<?php
-						// Start the loop.
+					<?php
+
+						/*
+							Created a custom field that work with php functions on ciranda.php
+							to solve the problem that setting offsets breaks pagination.
+
+						*/
+						$search= get_query_var('s');
+						$paged = (get_query_var("paged")) ? get_query_var("paged") : 1;
+						$args = array(
+								's' => $search,
+								'orderby' 		 => 'date',
+								'order' 		 => 'DESC',
+								'posts_per_page' => 8,
+								'paged' 		 => $paged,
+								'offset_start'	 => 0,         // custom field
+
+						);
 						$index = 0;
-						while ( have_posts() ) :
-							the_post();
-							?>
-							<a href="<?php the_permalink(); ?>">
-								<div class="latest-item box card card-decoration category-<?php
+						// The Query
+						$the_query = new WP_Query( $args );
+						global $wp_query;
+						// Put default query object in a temp variable
+						$tmp_query = $wp_query;
+						// Now wipe it out completely
+						$wp_query = null;
+						// Re-populate the global with our custom query
+						$wp_query = $the_query;
 
-								/* Banner Category Color*/
-								$categories = get_the_category();
-								echo $categories[0]->slug;
-								?>">
-									<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="latest-image">
-									<h2><?php echo get_the_title(); ?></h2>
-									<span class="author"><em>Por</em> <?php echo get_the_author(); ?></span>
-									<p class="excerpt">
-										<?php
-										$excerpt = get_the_post_summary(256);
+						// The Loop
+						if ( $the_query->have_posts() ) :
+							while ( $the_query->have_posts() ) :
+								$the_query->the_post();
+					?>
+					<a href="<?php the_permalink(); ?>">
+						<div class="latest-item box card card-decoration category-<?php
 
-										$excerpt = substr($excerpt, 0, 180);
-										echo $excerpt;
-										?>
-									</p>
-								</div>
-							</a>
-							<?php
+						/* Category Color*/
+						$categories = get_the_category();
+						echo $categories[0]->slug;
+						?>">
+							<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="latest-image">
+							<h2><?php echo get_the_title(); ?></h2>
+							<span class="author"><em>Por</em> <?php echo get_the_author(); ?></span>
+							<p class="excerpt">
+								<?php
+								$excerpt = get_the_post_summary(256);
 
+								$excerpt = substr($excerpt, 0, 180);
+								echo $excerpt;
+								?>
+							</p>
+						</div>
+					</a>
+						<?php
 							$index++;
 
-						endwhile;
+							endwhile;
 
-						wp_reset_postdata();
-
-						else :
-							echo "<h1>Nenhum resultado foi encontrado para: " .
-									'<span>' . get_search_query() . '</span>' .
-								"</h1>";
-						endif;
+							else :
+								// no posts found
+							endif;
 						?>
-				</main><!-- #main -->
+					</div>
+					<div class="col-12">
+
+						<?php
+
+						understrap_pagination(array(
+								'mid_size'  => 2,
+						));
+
+						// Restore original query object
+						$wp_query = null;
+						$wp_query = $tmp_query;
+						?>
+					</div>
+				</div>
 			</div>
+		</div>
+	</div>
 
-			<?php
-			// Display the pagination component.
-			understrap_pagination();
-			?>
-
-		</div><!-- .row -->
-
-	</div><!-- #content -->
-
-</div><!-- #search-wrapper -->
+</div><!-- #archive-wrapper -->
 
 <?php
 get_footer();
